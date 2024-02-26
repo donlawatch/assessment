64c2006bc323c0e5555d8ab4022a4c98f52d4ce0
donlawatch <donlawat.dc@gmail.com>
2024-02-26 19:50:22 +0700

    Remove unuse imports

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/dtos/responses/LotteryResponse.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/dtos/responses/LotteryResponse.java
@@ -1,10 +1,7 @@
 package com.kbtg.bootcamp.posttest.dtos.responses;
 
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
